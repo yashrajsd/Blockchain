addc24f539f9d138ccb2566f769619816f16e27a
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2024-12-17 10:05:22 +0530

    block class

---
+++ Block.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Block {
+    public String hash;
+    public String previousHash;
+    private String data;
+    private long timeStamp;
+    
+    public Block(String data,String previousHash){
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+    }
+}
