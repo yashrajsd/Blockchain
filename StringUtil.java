1a901b596cee8f201959ab806ea1d237d44eb792
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2024-12-17 17:04:48 +0530

    signature creation/verification added

--- StringUtil.java
+++ StringUtil.java
@@ -1,11 +1,16 @@
+import java.security.Key;
 import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.util.Base64;
 
 public class StringUtil {
     public static String applySha256(String input){
         try{
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(input.getBytes("UTF-8"));
-            StringBuffer hexString = new StringBuffer(); //to store hash
+            StringBuffer hexString = new StringBuffer(); 
             for(int i=0;i<hash.length;i++){
                 String hex = Integer.toHexString(0xff & hash[i]);
                 if(hex.length()==1) hexString.append('0');
@@ -16,4 +21,39 @@ public class StringUtil {
             throw new RuntimeException(e);
         }
     }
+    
+    //applying ECDSA signature
+    public static byte[] applyECDSASig(PrivateKey privateKey,String input){
+        Signature dsa;
+        byte[] output = new byte[0];
+
+        try{
+            dsa = Signature.getInstance("ECDSA","BC");
+            dsa.initSign(privateKey);
+            byte[] strByte = input.getBytes();
+            dsa.update(strByte);
+            byte[] realSig = dsa.sign();
+            output = realSig;
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+        return output;
+    }
+
+    //verifying the signature
+    public static boolean verifyECDSASig(PublicKey publicKey,String data, byte[] signature){
+        try{
+            Signature ecdsaVerify = Signature.getInstance("ECDSA","BC");
+            ecdsaVerify.initVerify(publicKey);
+            ecdsaVerify.update(signature);
+            return ecdsaVerify.verify(signature);
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getStringFromKey(Key key){
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
 }
